db8a69a4cc3496545c9cdc113375795b029d1157
Sleepapp <vontthao@gmail.com>
2024-12-19 12:40:10 +0700

    Change in group data

--- A.py
+++ A.py
@@ -8,17 +8,17 @@ from scipy.stats import zscore
 
 #Read data
 df_no_diagnosis = pd.read_csv('../RespiratorySound_proj/demographic_info.txt', 
-                               names = ['Patient number', 'Age', 'Sex', 'Adult BMI (kg/m2)', 'Child Weight (kg)', 'Child Height (cm)'], delimiter = ' ')
+                               names = ['Patient id', 'Age', 'Sex', 'Adult BMI (kg/m2)', 'Child Weight (kg)', 'Child Height (cm)'], delimiter = ' ')
+
 diagnosis = pd.read_csv('../RespiratorySound_proj/Respiratory_Sound_Database/Respiratory_Sound_Database/patient_diagnosis.csv', 
-                        names = ['Patient number', 'Diagnosis'])
-df = df_no_diagnosis.join(diagnosis.set_index('Patient number'), on='Patient number', how='left')
+                        names = ['Patient id', 'Diagnosis'])
 
 path='../RespiratorySound_proj/Respiratory_Sound_Database/Respiratory_Sound_Database/audio_and_txt_files/'
 files=[s.split('.')[0] for s in os.listdir(path) if '.txt' in s]
 
 def Extract_Annotation_Data(file_name, root):
     tokens = file_name.split('_')
-    recording_info = pd.DataFrame(data = [tokens], columns = ['Patient number', 'Recording index', 'Chest location','Acquisition mode','Recording equipment'])
+    recording_info = pd.DataFrame(data = [tokens], columns = ['Patient id', 'Recording index', 'Chest location','Acquisition mode','Recording equipment'])
     recording_annotations = pd.read_csv(os.path.join(root, file_name + '.txt'), names = ['Start', 'End', 'Crackles', 'Wheezes'], delimiter= '\t')
     return (recording_info, recording_annotations)
 
@@ -31,43 +31,50 @@ for s in files:
     rec_annotations.append(a)
     rec_annotations_dict[s] = a
 recording_info = pd.concat(i_list, axis = 0)
-print(recording_info.tail())
-print(recording_info.info())
-recording_info = recording_info.apply(pd.to_numeric, errors='coerce')
-'''
-print("Display diagnosis data first 5 rows:")
-print(df_no_diagnosis.head())  
-print("\nDisplay diagnosis data first 5 rows:")
-print(diagnosis.head()) 
+recording_info['Patient id'] = recording_info['Patient id'].apply(pd.to_numeric, errors='coerce')
 
-print("\nCombined data first 5 rows:")
-print(df.head())
+df = diagnosis.join(recording_info.set_index('Patient id'), on='Patient id', how='left')
 
-print("\nDiagnosis value counts:")
-print(df['Diagnosis'].value_counts())
+adults = df_no_diagnosis[df_no_diagnosis['Age'] >= 18].drop(['Child Weight (kg)', 'Child Height (cm)'], axis=1)
+children = df_no_diagnosis[df_no_diagnosis['Age'] < 18].drop(['Adult BMI (kg/m2)'], axis=1)
+adults = adults.join(df.set_index('Patient id'), on='Patient id', how='left')
+children = children.join(df.set_index('Patient id'), on='Patient id', how='left')
 
-print("\nCheck missing values:")
-print(df.isnull().sum())
+print("\nShow first 5 rows for adults dataset:")
+print(adults.head())
+print("\nShow first 5 rows for children dataset:")
+print(children.head())
 
-#Add values to missing
-df = df.fillna(df.mean(numeric_only=True))
-df['Sex'] = df['Sex'].fillna('Unknown')
+print("\nData types for adults dataset:")
+print(adults.info())
+print("\nData types for children dataset:")
+print(children.info())
 
-print("\nCheck duplicated values:")
-print(df[df.duplicated(keep=False)].sum())
+print("\nCheck missing values for adults dataset:")
+print(adults.isnull().sum())
+print("\nCheck missing values for children dataset:")
+print(children.isnull().sum())
 
-print("\nInfomation of data:")
-print(df.info())
+#Add values to missing values
+children = children.fillna(children.mean(numeric_only=True))
+adults = adults.fillna(adults.mean(numeric_only=True))
+
+print("\nCheck duplicated values for adults dataset:")
+print(adults[adults.duplicated(keep=False)].sum())
+print("\nCheck duplicated values for children dataset:")
+print(children[children.duplicated(keep=False)].sum())
+
 
 print("\nDescibe the data:")
-print(df.describe().T)
-
+print(adults.describe().T)
+print(children.describe().T)
+'''
 print("\nCheck outliers")
 #Boxplot before cleaned
-continuous = ['Patient number', 'Age', 'Adult BMI (kg/m2)', 'Child Weight (kg)', 'Child Height (cm)']
+continuous = ['Patient id', 'Age', 'Adult BMI (kg/m2)', 'Child Weight (kg)', 'Child Height (cm)']
 fig, axes = plt.subplots(len(continuous), 1, figsize=(10, len(continuous) * 2))
 for i, ax in zip(continuous, axes):
-    sns.boxplot(x=df[i], color='#A4161A', linewidth=1, ax=ax)
+    sns.boxplot(x=df1[i], color='#A4161A', linewidth=1, ax=ax)
     ax.set_title(f'Boxplot of {i}')
     ax.set_xlabel(i)
     ax.set_ylabel('')
@@ -77,17 +84,17 @@ plt.tight_layout()
 #Z-score to delete outliers
 Z_THRESHOLD = 3
 for col in continuous:
-    if df[col].dtype in ['float64', 'int64']:
-        df_clean = df[(abs(zscore(df[col].dropna())) <= Z_THRESHOLD)]
+    if df1[col].dtype in ['float64', 'int64']:
+        df1_clean = df1[(abs(zscore(df1[col].dropna())) <= Z_THRESHOLD)]
 
 print("Data after removing outliers:")
-print(df_clean.describe().T)
+print(df1_clean.describe().T)
 
 #Boxplot after cleaned
 fig, axes = plt.subplots(len(continuous), 1, figsize=(10, len(continuous) * 2))
 
 for i, ax in zip(continuous, axes):
-    sns.boxplot(x=df_clean[i], color='#A4161A', linewidth=1, ax=ax)
+    sns.boxplot(x=df1_clean[i], color='#A4161A', linewidth=1, ax=ax)
     ax.set_title(f'Boxplot of {i} (Outliers removed)')
     ax.set_xlabel(i)
     ax.set_ylabel('')
